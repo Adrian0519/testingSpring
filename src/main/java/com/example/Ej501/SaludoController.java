e740eb71d886f5a7e921027db60974164aba59bf
Mateo <mateocarballo2@gmail.com>
2026-03-11 22:27:35 +0100

    Añadida la base de datos a 'application.properties' para que funcione

--- src/main/java/com/example/Ej501/SaludoController.java
+++ src/main/java/com/example/Ej501/SaludoController.java
@@ -1,6 +1,5 @@
 package com.example.Ej501;
 
-import com.example.Ej501.SaludoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
